3fa3cd5aefaf295f6a75945d832de49f84b9caab
Nicolleee! <128643495+Nicolle07M@users.noreply.github.com>
2025-04-08 18:54:58 -0500

    feat(Backend): migrations full

--- fotologybackend/database/migrations/2025_04_06_144411_create_photography_table.php
+++ fotologybackend/database/migrations/2025_04_06_144411_create_photography_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePhotographiesTable extends Migration
+class CreatePhotographyTable extends Migration
 {
     /**
      * Run the migrations.
